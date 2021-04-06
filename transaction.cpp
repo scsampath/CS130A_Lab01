b46d20fee3207cdffe2220f5a7971eae62f766c4
Sachen Sampath <scsampath@cs.ucsb.edu>
2021-04-05 17:05:56 -0700

    commit

--- transaction.cpp
+++ transaction.cpp
@@ -9,20 +9,20 @@
 #include <iostream> // f o r t ime ( )
 
 // adapted from: https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c/10632725
-string SHA256(const string str)
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
-    SHA256_Final(hash, &sha256);
-    stringstream ss;
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        ss << hex << setw(2) << setfill('0') << (int)hash[i];
-    }
-    return ss.str();
-}
+// string SHA256(const string str)
+// {
+//     unsigned char hash[SHA256_DIGEST_LENGTH];
+//     SHA256_CTX sha256;
+//     SHA256_Init(&sha256);
+//     SHA256_Update(&sha256, str.c_str(), str.size());
+//     SHA256_Final(hash, &sha256);
+//     stringstream ss;
+//     for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+//     {
+//         ss << hex << setw(2) << setfill('0') << (int)hash[i];
+//     }
+//     return ss.str();
+// }
 
 char randChar() {
     return char(rand() % 26 + 'a');
